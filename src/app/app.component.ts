cfd52f76750176503e57e25e2f8fc921a4da250d
Hassan <hassansyed123@hotmail.com>
2024-05-06 19:14:07 -0400

    Added list with filter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WishItem } from '../shared/models/wishItem';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { filter } from 'rxjs';
+
+
+const filters = [
+  (item : WishItem) => item,
+  (item : WishItem) => !item.isComplete,
+  (item : WishItem) => item.isComplete
+]
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items : WishItem[] = [
+    new WishItem('To Learn Angular'),
+    new WishItem('Get Coffee', true),
+    new WishItem('Find grass that cuts itself')
+  ];
+
+  listFilter : any = '0'
+
+  newWishText = '';
+
   title = 'wishlist';
+
+  get visibleItems() : WishItem[] {
+    return this.items.filter(filters[this.listFilter])
+  };
+
+  addNewWish() {
+    // todo: add the wish to the items array
+    // clear the textbox
+    this.items.push(new WishItem(this.newWishText));
+    this.newWishText = '';
+
+
+  }
+
+
+  toggleItem(item : WishItem) {
+    item.isComplete = !item.isComplete
+    console.log(item)
+  }
 }
