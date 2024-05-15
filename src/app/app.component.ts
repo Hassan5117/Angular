d72d9484c17172c029a0e5efa15eba7331eea247
Hassan <hassansyed123@hotmail.com>
2024-05-15 17:05:37 -0400

    Issued Http request to local json file to retrieve wishes. Provided options for Http requests, and provided error handling for development and users.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { WishItem } from '../shared/models/wishItem';
 import { CommonModule } from '@angular/common';
@@ -8,6 +8,7 @@ import { AddWishFormComponent } from './add-wish-form/add-wish-form.component';
 import { WishFilterComponent } from './wish-filter/wish-filter.component';
 import { EventService } from '../shared/services/EventService';
 import { HttpClientModule } from '@angular/common/http';
+import { WishService } from './wish.service';
 
 
 @Component({
@@ -17,20 +18,31 @@ import { HttpClientModule } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  items : WishItem[] = [
-    new WishItem('To Learn Angular'),
-    new WishItem('Get Coffee', true),
-    new WishItem('Find grass that cuts itself')
-  ];
+export class AppComponent implements OnInit {
+  items : WishItem[] = [];
+  // items : WishItem[] = [
+  //   new WishItem('To Learn Angular'),
+  //   new WishItem('Get Coffee', true),
+  //   new WishItem('Find grass that cuts itself')
+  // ];
 
-  constructor(events: EventService) {
+  constructor(events: EventService, private wishService: WishService) {
     events.listen('removeWish', (wish : any) => {
       //remove wish from items
       let index = this.items.indexOf(wish)
       this.items.splice(index, 1)
       
     })
+  }
+
+  ngOnInit(): void {
+    this.wishService.getWishes().subscribe((data : any) => {
+      this.items = data;
+    },
+    (error: any) => {
+      alert(error.message);
+    }
+  );
   }
 
   filter : any;
---
+++ src/app/wish.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WishItem } from '../shared/models/wishItem';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'any'
+})
+export class WishService {
+
+  constructor(private http: HttpClient) { }
+
+  private getStandardOptions() : any {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      })
+    };
+  }
+
+  getWishes() {
+    let options = this.getStandardOptions();
+    options.params = new HttpParams({
+      fromObject: {
+        format: 'json'
+      }
+    });
+
+    return this.http.get('assets/wishes1.json', options).pipe(catchError(this.handleError));
+
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status == 0) {
+      console.error('There is an issue with the lient or network: ', error.error)
+    }
+    else {
+      console.error('Server-side error: ', error.error)
+    }
+
+    return throwError(() => new Error('Cannot retrieve wishes from the server, please try again'))
+  }
+
+  private addWish(wish: WishItem) {
+    let options = this.getStandardOptions()
+
+    options.headers = options.headers.set('Authorization', 'value-need-for-authorization')
+    this.http.post('assets/wishes.json', wish, options)
+
+  }
+  
+
+}
