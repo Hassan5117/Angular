298c396727dc1e17ef464b3e1a3d27c95ed250ca
Hassan <hassansyed123@hotmail.com>
2024-05-16 12:57:48 -0400

    Created Contact Form and used ReactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { WishModule } from './wish/wish.module';
+import { ContactModule } from './contact/contact.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, WishModule],
+  imports: [RouterOutlet, CommonModule, WishModule, ContactModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+
+  senderNameControl = new FormControl('');
+  senderEmailControl = new FormControl('');
+  senderMessageControl = new FormControl('');
+
+  submitForm() {
+    if(this.senderNameControl.dirty) {
+      alert('you changed the name field')
+    }
+  }
+}
